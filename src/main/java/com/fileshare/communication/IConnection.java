7dbaf92df47b7c6a1b2882dc2da6c08f2cb0711e
Jan Paw <explicite@windowslive.com>
2013-06-02 20:22:21 +0200

    Network 6.2.2013

--- src/main/java/com/fileshare/communication/IConnection.java
+++ src/main/java/com/fileshare/communication/IConnection.java
@@ -1,13 +1,14 @@
 package com.fileshare.communication;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 /**
  * @author Jan Paw
  *         Date: 5/30/13
  */
 public interface IConnection extends Remote {
-    public Message receive();
+    public Message receive() throws RemoteException;
 
-    public void send(Message message);
+    public void send(Message message) throws RemoteException;
 }
