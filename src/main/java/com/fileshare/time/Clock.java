aca6e5ff27686794bedc921973debd881b6039f9
Mateusz Kwiecien <kwiecien.mateusz.89@gmail.com>
2013-06-06 08:46:48 +0200

    Clock

--- src/main/java/com/fileshare/time/Clock.java
+++ src/main/java/com/fileshare/time/Clock.java
@@ -1,10 +1,117 @@
 package com.fileshare.time;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 /**
- * User: Jan Paw
- * Date: 5/30/13
+ * @author Mateusz Kwiecien
+ *         Date: 5/30/13
  */
 
-//TODO Mateusz
-public class Clock {
+public class Clock implements IClock {
+    private Map<Integer, Integer> vector = new HashMap<Integer, Integer>();
+    private int nodeId;
+
+    public Clock(Integer id) {
+        this.nodeId = id;
+        this.vector.put(id, 0);
+    }
+
+    @Override
+    public int getNodeId() {
+        return nodeId;
+    }
+
+    @Override
+    public void setNodeId(int clientId) {
+        this.nodeId = clientId;
+    }
+
+    @Override
+    public void addNode(Integer id, Integer state) {
+        if (!vector.containsKey(id)) {
+            vector.put(id, state);
+        }
+    }
+
+    @Override
+    public Map<Integer, Integer> getVector() {
+        return vector;
+    }
+
+    @Override
+    public void setVector(Map<Integer, Integer> vector) {
+        this.vector = vector;
+    }
+
+    @Override
+    public int getSize() {
+        return vector.size();
+    }
+
+    @Override
+    public void incrementClock() {
+        if (vector.containsKey(this.nodeId)) {
+            vector.put(this.nodeId, (vector.get(this.nodeId) + 1));
+        }
+    }
+
+    @Override
+    public boolean equivalent(Clock current) {
+        return false;  //TODO
+    }
+
+    @Override
+    public boolean isGreater(Clock current) {
+        if (current.equals(this)) {
+            return false;
+        } else {
+            Iterator it = current.vector.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry pairs = (Map.Entry) it.next();
+                Integer get = this.vector.get(((Integer) pairs.getKey()));
+                if (!((Integer) pairs.getValue() < get)) {
+                    return false;
+                }
+                it.remove();
+            }
+
+            return true;
+        }
+
+    }
+
+    @Override
+    public boolean isLower(Clock current) {
+        return false;  //TODO
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (super.equals(obj)) {
+            return true;
+        }
+
+        Clock current = (Clock) obj;
+
+        if (current.vector.size() == this.vector.size()) {
+
+            Iterator it = current.vector.entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry pairs = (Map.Entry) it.next();
+                Integer get = this.vector.get(((Integer) pairs.getKey()));
+                if (pairs.getValue() != get) {
+                    return false;
+                }
+                it.remove();
+            }
+            return true;
+        } else {
+            return false;
+        }
+
+    }
 }
+
---
+++ src/main/java/com/fileshare/time/IClock.java
@@ -0,0 +1,20 @@
+package com.fileshare.time;
+
+import java.util.Map;
+
+/**
+ * @author Mateusz Kwiecien
+ *         Date: 06.06.13
+ */
+public interface IClock {
+    public int getNodeId();
+    public void setNodeId(int clientId);
+    public void addNode(Integer id, Integer state);
+    public Map<Integer, Integer> getVector();
+    public void setVector(Map<Integer, Integer> vector);
+    public int getSize();
+    public void incrementClock();
+    public boolean equivalent(Clock current);
+    public boolean isGreater(Clock current);
+    public boolean isLower(Clock current);
+}
