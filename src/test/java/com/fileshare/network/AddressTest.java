71123d3e49dd7acc47857ab92095ab56cb6a027c
Jan Paw <explicite@windowslive.com>
2013-06-13 12:21:02 +0200

    issue #20

---
+++ src/test/java/com/fileshare/network/AddressTest.java
@@ -0,0 +1,46 @@
+package com.fileshare.network;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Jan Paw
+ *         Date: 6/13/13
+ */
+public class AddressTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetInetAddress() throws Exception {
+
+    }
+
+    @Test
+    public void testGetName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAddress() throws Exception {
+
+    }
+
+    @Test
+    public void testIsReachable() throws Exception {
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+}
