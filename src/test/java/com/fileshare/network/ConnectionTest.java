193added8950bfcc5a3b107857a0803152b711f4
Jan Paw <explicite@windowslive.com>
2013-06-13 12:20:25 +0200

    issue #21

---
+++ src/test/java/com/fileshare/network/ConnectionTest.java
@@ -0,0 +1,41 @@
+package com.fileshare.network;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Jan Paw
+ *         Date: 6/13/13
+ */
+public class ConnectionTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testIsReachable() throws Exception {
+
+    }
+
+    @Test
+    public void testUpload() throws Exception {
+
+    }
+
+    @Test
+    public void testDownload() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAddress() throws Exception {
+
+    }
+}
