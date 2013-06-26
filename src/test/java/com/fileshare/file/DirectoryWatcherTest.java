3ee4aafc1cf9ef6d47a425af7d655088f6877f11
johnnyfiftyfive <sikorekk@gmail.com>
2013-06-26 21:29:46 +0200

    Modyfikacja DirWatchera

--- src/test/java/com/fileshare/file/DirectoryWatcherTest.java
+++ src/test/java/com/fileshare/file/DirectoryWatcherTest.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -25,33 +25,38 @@ public class DirectoryWatcherTest {
     @Before
     public void createDirectoryWatcher() {
         clock = new Clock("aa");
-        directoryWatcher = new DirectoryWatcher("./test", 1, clock);
+        directoryWatcher = new DirectoryWatcher("./test", 3, clock);
 
         eventObserver = new EventObserver();
         directoryWatcher.addObserver(eventObserver);
-
-        new Thread(directoryWatcher).start();
+        directoryWatcher.watchDir();
+        //  new Thread(directoryWatcher).start();
     }
 
     @Test
-    public void doesItEvenWorkTest(){
-        File dummy = DummyFile.generateFile("./test", 5096);
-        while(eventObserver.getCaughtEvent() == 0){}
-        System.out.println(eventObserver.caughtEvent);
+    public void doesItEvenWorkTest() {
+        testFile = DummyFile.generateFile("./test", 5096);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        while (eventObserver.getCaughtEvent() == 0)
+            testFile.delete();
+        assertTrue(eventObserver.getCaughtEvent() > 0);
     }
 
     @Test
     public void observerTest() {
-       /* testFile = DummyFile.generateFile("./test", 1024);
-
         while (eventObserver.getCaughtEvent() == 0) {
+            testFile = DummyFile.generateFile("./test", 1024);
         }
-        assertTrue(eventObserver.getCaughtEvent() == FileInfo.FLAG_CREATED);*/
+        assertTrue(eventObserver.getCaughtEvent() == FileInfo.FLAG_CREATED);
     }
 
     @After
     public void clean() {
-//        testFile.delete();
+        testFile.delete();
     }
 
     private class EventObserver implements Observer {
@@ -63,9 +68,10 @@ public class DirectoryWatcherTest {
 
         @Override
         public void update(Observable o, Object arg) {
-            ArrayList<FileInfo> changes = (ArrayList<FileInfo>) arg;
-            FileInfo pathInfo = changes.get(0);
-            caughtEvent = pathInfo.getFlag();
+            HashMap<String, FileInfo> changes = (HashMap<String, FileInfo>) arg;
+            for (FileInfo fileInfo : changes.values()) {
+                caughtEvent = fileInfo.getFlag();
+            }
         }
     }
 }
